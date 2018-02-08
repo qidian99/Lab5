828ed83608938c3538b2c165f369a0bb612c315e
qidian99 <qidian990107@163.com>
2018-02-07 16:15:50 -0800

    Hello uses Greeter

--- lib/Hello.java
+++ lib/Hello.java
@@ -3,12 +3,11 @@ public class Hello
     public static void main( String argv[] )
     {
 	//Default is "World"
-	//Author: Jim Weirich (jim@somewhere.com)
         String name = "World";
 	if(argv.length != 0)
 	{
           name = argv[0];
 	}
-	System.out.println( "Hello, " + name + "!" );
-    }
+	Greeter greeter = new Greeter(name);
+	System.out.println(greeter.greet());
 }
